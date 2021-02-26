bd1b1cb8e304323a54cd854de3544e510446bfc0
LukasHolm420 <lukasholm@hotmail.com>
2021-02-26 04:35:12 +0100

    improved roll function

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from tkinter import *
 from random import seed
 from random import sample
+from random import randint
+import time
 
 root = Tk()
 root.title('Dice_Roller')
 root.iconbitmap(r"icon.ico")
 root.geometry("400x200")
 
-seed(1)
+seed(time.time())
+
 
 def dice_roller():
-    possible_rolls = [i for i in range(die.get() + 1)] *10
-    possible_rolls = [i for i in possible_rolls if i != 0]
-    rolls = sample(possible_rolls, e.get())
+    rolls = []
+    i = 0
+    while i < e.get():
+        rolls.append(randint(1, die.get()))
+        i += 1
     myLabel.config(text=rolls)
-    return 0
+
+    pass
+
 
 die = IntVar()
 
-drop = OptionMenu(root, die, 4, 6, 12, 20,)
+drop = OptionMenu(root, die, 4, 6, 12, 20, )
 drop.pack()
 
 die.set(6)
@@ -26,12 +33,10 @@ die.set(6)
 e = Scale(root, from_=1, to=50, orient=HORIZONTAL)
 e.pack()
 
-myButton = Button(root, text="Roll!", command = dice_roller)
+myButton = Button(root, text="Roll!", command=dice_roller)
 myButton.pack()
 
 myLabel = Label(root, text="Dice rolls wil appear here")
 myLabel.pack()
 
-
 root.mainloop()
-
